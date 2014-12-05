6a5d4364ad84e3f2f929a48c517f92bc50ecb7b7
lemonbar <meng_li@ge.com>
2014-12-05 14:27:07 +0800

    add another upload file method for http

--- src/main/java/com/lemon/weixin/sdk/util/WXHttpUtil.java
+++ src/main/java/com/lemon/weixin/sdk/util/WXHttpUtil.java
@@ -11,8 +11,9 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
 
 /**
@@ -116,5 +117,96 @@ public class WXHttpUtil {
             }
         }
         return null;
+    }
+
+    public String sendFile(String url, String filePath) throws IOException {
+        String result = null;
+
+        File file = new File(filePath);
+        if (!file.exists() || !file.isFile()) {
+            throw new IOException("文件不存在");
+        }
+
+        /**
+         * 第一部分
+         */
+        URL urlObj = new URL(url);
+        // 连接
+        HttpURLConnection con = (HttpURLConnection) urlObj.openConnection();
+
+        /**
+         * 设置关键值
+         */
+        con.setRequestMethod("POST"); // 以Post方式提交表单，默认get方式
+        con.setDoInput(true);
+        con.setDoOutput(true);
+        con.setUseCaches(false); // post方式不能使用缓存
+
+        // 设置请求头信息
+        con.setRequestProperty("Connection", "Keep-Alive");
+        con.setRequestProperty("Charset", "UTF-8");
+
+        // 设置边界
+        String BOUNDARY = "----------" + System.currentTimeMillis();
+        con.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + BOUNDARY);
+
+        // 请求正文信息
+        // 第一部分：
+        StringBuilder sb = new StringBuilder();
+        sb.append("--"); // 必须多两道线
+        sb.append(BOUNDARY);
+        sb.append("\r\n");
+        sb.append("Content-Disposition: form-data;name=\"file\";filename=\""
+                + file.getName() + "\"\r\n");
+        sb.append("Content-Type:application/octet-stream\r\n\r\n");
+
+        byte[] head = sb.toString().getBytes("utf-8");
+
+        // 获得输出流
+        OutputStream out = new DataOutputStream(con.getOutputStream());
+        // 输出表头
+        out.write(head);
+
+        // 文件正文部分
+        // 把文件已流文件的方式 推入到url中
+        DataInputStream in = new DataInputStream(new FileInputStream(file));
+        int bytes = 0;
+        byte[] bufferOut = new byte[1024];
+        while ((bytes = in.read(bufferOut)) != -1) {
+            out.write(bufferOut, 0, bytes);
+        }
+        in.close();
+
+        // 结尾部分
+        byte[] foot = ("\r\n--" + BOUNDARY + "--\r\n").getBytes("utf-8");// 定义最后数据分隔线
+
+        out.write(foot);
+        out.flush();
+        out.close();
+
+        StringBuffer buffer = new StringBuffer();
+        BufferedReader reader = null;
+        try {
+            // 定义BufferedReader输入流来读取URL的响应
+            reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                //System.out.println(line);
+                buffer.append(line);
+            }
+            if (result == null) {
+                result = buffer.toString();
+            }
+        } catch (IOException e) {
+            System.out.println("发送POST请求出现异常！" + e);
+            e.printStackTrace();
+            throw new IOException("数据读取异常");
+        } finally {
+            if (reader != null) {
+                reader.close();
+            }
+        }
+
+        return result.toString();
     }
 }
