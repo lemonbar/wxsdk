c5dcc13932c2c911f3029fa4201f20fd39b6463b
lemon_bar <lemon_bar@163.com>
2015-07-23 18:05:49 +0800

    update code

---
+++ src/test/java/com/kunbao/weixin/sdk/management/user/WXUserServiceTest.java
@@ -0,0 +1,13 @@
+package com.kunbao.weixin.sdk.management.user;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lemon_bar on 15/7/23.
+ */
+public class WXUserServiceTest extends TestCase {
+
+    public void testCreateUserGroup() throws Exception {
+
+    }
+}
