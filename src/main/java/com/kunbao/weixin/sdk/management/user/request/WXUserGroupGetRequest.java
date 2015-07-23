0b3fece098dc70bede2b983fc6fb7a23edc3b20c
lemon_bar <lemon_bar@163.com>
2015-07-24 00:25:45 +0800

    update code

---
+++ src/main/java/com/kunbao/weixin/sdk/management/user/request/WXUserGroupGetRequest.java
@@ -0,0 +1,7 @@
+package com.kunbao.weixin.sdk.management.user.request;
+
+/**
+ * Created by lemon_bar on 15/7/23.
+ */
+public class WXUserGroupGetRequest {
+}
---
+++ src/main/java/com/kunbao/weixin/sdk/management/user/response/WXUserGroupGetIdResponse.java
@@ -0,0 +1,7 @@
+package com.kunbao.weixin.sdk.management.user.response;
+
+/**
+ * Created by lemon_bar on 15/7/23.
+ */
+public class WXUserGroupGetIdResponse {
+}
