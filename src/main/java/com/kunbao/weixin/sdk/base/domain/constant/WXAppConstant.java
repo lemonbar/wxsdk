3e9757bb8f5be1272f480f5ef9a794a14b4d84ec
lemon_bar <lemon_bar@163.com>
2015-07-23 12:22:25 +0800

    update files.

--- src/main/java/com/kunbao/weixin/sdk/base/domain/constant/WXAppConstant.java
+++ src/main/java/com/kunbao/weixin/sdk/base/domain/constant/WXAppConstant.java
@@ -4,10 +4,10 @@ package com.kunbao.weixin.sdk.base.domain.constant;
  * Created by lemon_bar on 15/7/23.
  */
 public class WXAppConstant {
-    public static String TOKEN = "DNXX2XDF";
-    public static String APP_ID = "wx4f135cf07185a290";
-    public static String APP_SECRET = "4b8bffa34e67d63f8a7f080752a36c40";
-    public static String EncodingAESKey = "UyiZ5eWawdxA6cNbMTwyMKSu9nUDOutrbkDLhbxGULS";
+    public static String TOKEN = "XXXX";
+    public static String APP_ID = "XXXXX";
+    public static String APP_SECRET = "XXXX";
+    public static String EncodingAESKey = "XXX";
 
     public static void init(String appId, String appSecret, String appToken, String encodingAESKey) {
         APP_ID = appId;
