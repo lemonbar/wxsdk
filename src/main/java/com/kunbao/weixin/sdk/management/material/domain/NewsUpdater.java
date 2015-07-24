7c9ce566bb3b10273c1f32d385436af69be4038d
lemon_bar <lemon_bar@163.com>
2015-07-24 10:58:53 +0800

    update

---
+++ src/main/java/com/kunbao/weixin/sdk/management/material/domain/NewsUpdater.java
@@ -0,0 +1,7 @@
+package com.kunbao.weixin.sdk.management.material.domain;
+
+/**
+ * Created by lemon_bar on 15/7/24.
+ */
+public class NewsUpdater {
+}
---
+++ src/main/java/com/kunbao/weixin/sdk/management/material/request/WXUpdateNewsRequest.java
@@ -0,0 +1,7 @@
+package com.kunbao.weixin.sdk.management.material.request;
+
+/**
+ * Created by lemon_bar on 15/7/24.
+ */
+public class WXUpdateNewsRequest {
+}
