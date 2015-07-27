e89d7749d22ddc88755bd3a1910d359ea4171145
lemon_bar <lemon_bar@163.com>
2015-07-27 23:53:29 +0800

    update

---
+++ src/main/java/com/kunbao/weixin/sdk/datacube/user/WXUserDataCubeService.java
@@ -0,0 +1,7 @@
+package com.kunbao.weixin.sdk.datacube.user;
+
+/**
+ * Created by lemon_bar on 15/7/27.
+ */
+public class WXUserDataCubeService {
+}
---
+++ src/test/java/com/kunbao/weixin/sdk/datacube/user/WXUserDataCubeServiceTest.java
@@ -0,0 +1,13 @@
+package com.kunbao.weixin.sdk.datacube.user;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by lemon_bar on 15/7/27.
+ */
+public class WXUserDataCubeServiceTest extends TestCase {
+
+    public void testGetUserSummary() throws Exception {
+
+    }
+}
